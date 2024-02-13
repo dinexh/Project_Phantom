672ef798d7d10908b9f386352fc0ca1b114511a2
Dinesh korukonda <dineshkorukonda05@gmail.com>
2024-02-13 17:58:37 +0530

    Counter Done

--- Client/script.js
+++ Client/script.js
@@ -1,19 +1,25 @@
-const counters = document.querySelectorAll('.counters');
-const speed = 50;
-
-counters.forEach(counter => {
-  const updateValue = () => {
-    const target = +counter.getAttribute('data-target');
-    const lastValue = +counter.innerText; 
-    const inc = target / speed;
-    
-    if (lastValue > target) {
-      counter.innerText = target;
-    }else {
-      counter.innerText = lastValue+inc;
-      setTimeout( updateValue, 1);
-    }
-  }
+function animateCounter(targetCounts, duration) {
+  const counters = document.querySelectorAll('.count');
   
-  updateValue();
-})
+  const increments = [];
+  counters.forEach((counter, index) => {
+    increments[index] = Math.ceil(targetCounts[index] / (duration * 10));
+  });
+  
+  const interval = setInterval(() => {
+    let stop = true;
+    counters.forEach((counter, index) => {
+      if (parseInt(counter.textContent) < targetCounts[index]) {
+        counter.textContent = parseInt(counter.textContent) + increments[index];
+        stop = false;
+      }
+    });
+
+    if (stop) {
+      clearInterval(interval);
+    }
+  }, 100); // Update every 100 milliseconds
+}
+
+// Call the animateCounter function with the target counts and duration
+animateCounter([3, 10, 10, 10], 5); // Count from 0 to 3, 10, 10, 10 in 5 seconds
